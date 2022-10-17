31f70673db982ab67b3d554d9b46d3bf15ad6918
ampaignitious <ignitiousampa08@gmail.com>
2022-10-18 01:41:12 +0300

    added new files

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -84,6 +84,15 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         //
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+        //
+        $product->update($request->all());
+        //
+        return redirect()->route('products.index')->with('success','Product updated successfully');
+
 
     }
 
@@ -96,5 +105,7 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         //
+        $product->delete();
+        return redirect()->route('products.index')->with('sucess','Product deleted successfully');
     }
 }
